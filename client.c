60c5be1cf8b39c7eafeb0b4d1518728a1ca61dec
atharvasg <atharvasg@gmail.com>
2016-02-15 16:30:28 -0800

    Final Code
    Final Code

--- client.c
+++ client.c
@@ -5,166 +5,47 @@
 #include<netdb.h>
 #include<string.h>
 #include<stdlib.h>
-#define WINDOWSIZE 3
-#define NAMESIZE 60
-#define SEQNO 1
-#define DATA  196
-#define MAX 200
-#define PORT 43455
-//window will be array of pointers  for struct  packet.
-struct packet
-{
-	int seqno;
-	char data[DATA];
-// 	char data[MAX-sizeof(int)];
-};
-/*struct 
 
-*/
- 
-unsigned long int getFileSize(FILE *fp)
+#define MAX 256
+#define PORT 43454
+void communicate(int socketdescriptor)
 {
-	unsigned long int filesize=0; 
-	fseek(fp, 0L, SEEK_END);
-	filesize = ftell(fp);
-	fseek(fp,0L,SEEK_SET);
- return filesize;
-}
-int getnoofPackets(unsigned long int filesize)
-{
-  int noofpackets=0;
- 	noofpackets=filesize/MAX;
-	if(filesize%MAX==0)
-	{
-		return noofpackets;	
-	}
-	else
-	{
-		return noofpackets+1;
-	}
-}
-struct packet createPacket(int type,char *data)
-{
-	struct packet NewPacket;	
-	char * packetdata=(char *)malloc(sizeof(DATA));
-	strcpy(packetdata,data);
-  	switch(type)
-  	{
-		case 1:
-			NewPacket.seqno=0;
-			strcpy(NewPacket.data,packetdata);
+	char buffer[MAX];
+	int n;
+	for(;;)
+	 {
+		memset(buffer,0,sizeof(buffer));		
+		printf("To Server : ");
+		n=0;
+		while((buffer[n++]=getchar())!='\n');
+		write(socketdescriptor,buffer,sizeof(buffer));
+		memset(buffer,0,sizeof(buffer));
+		read(socketdescriptor,buffer,sizeof(buffer));
+		printf("\t\t\tFrom Server : %s",buffer);
+		if((strncmp(buffer,"exit",4))==0)
+		 {
+			printf("Client Exit...\n");
 			break;
-		case 2:
-			break;
- 	}
-	printf("Packet Data is:%s\n",NewPacket.data);
-  return NewPacket;
-}
-char * readData(FILE *fp)
-{
-  	int i=0;
-  	char c;
-	char *buffer;
-	buffer=(char *)malloc(DATA);
-	while(c=fgetc(fp)!=EOF)	 
-   	{	
-		buffer[i++]=c;
-		if(i==DATA)
-		{
-			break;	
-		}
-   	}
-	buffer[i]='\0';
- printf("Printing data size from readData():- %ld\n",sizeof(buffer));
- return buffer;
-}
-
-void communicate(int socketdescriptor,struct sockaddr_in server,int len)
-{
-	unsigned long int filesize=0; 	
-	int noofpackets,n,a;
-	static int frameNo=0;
-	struct packet init;
-	char filename[NAMESIZE];
-	FILE *fp;
-	printf("Enter the file name to be sent:");
-	scanf("%s",filename);
-        noofpackets=n=a=0;
-	init=createPacket(1,filename);
-	a=sendto(socketdescriptor,(const void *)&init,sizeof(struct packet),0,(struct sockaddr *)&server,len);
-//	if(a>0)
-//	{
-        	/*fp=fopen(filename,"r");
-		if(fp>0)
-		{			
-			filesize=getFileSize(fp);
-			noofpackets=getnoofPackets(filesize);
-			while(fgetc(fp)!=EOF)
-			{
-				
-			}
-		}
-		else
-			printf("Error while reading the file!!\n");*/
-		for(;;)
-		{
-		  		
-		}
-/*		for(;;)
-		{
- 			frameno=fread(buffer,sizeof(buffer),1,fp);
-			while(frameno>0 && i<WINDOWSIZE)
-			{			
-				a = sendto(socketdescriptor,buffer,sizeof(buffer),0,(struct sockaddr *)&server,len);			//step 3 - sending the data to server.
-				if(a>0)
-					i++;
-				else
-				{
-					printf("Problem in network!\n");
-					break;				
-				}
-			}		
-			printf("Frame No: %d transferred.\n",frameno);	
-		}
-        	for(;;)
-	 	{          
-			memset(buffer,0,sizeof(buffer));		
-			printf("To Server: ");
-			n=0;
-			while((buffer[n++]=getchar())!='\n');
-			a = sendto(socketdescriptor,buffer,sizeof(buffer),0,(struct sockaddr *)&server,len);			//step 3 - sending the data to server.
-			memset(buffer,0,sizeof(buffer));
-			a = recvfrom(socketdescriptor,buffer,sizeof(buffer),0,(struct sockaddr *)&server,len);			//step 4 - reading the data sent by server
-			printf("Received Message:%s/n",buffer);
-			printf("\t\t\tFrom Server : %s",buffer);
-			if((strncmp(buffer,"exit",4))==0)
-			 {
-				printf("Client will Exit...End of communication\n");
-				break;
-			 }
-		 }*/
-//	}
-//	else
-//	{
-//	  printf();
-//	}
+		 }
+	 }
 }
 
 int main(int argc,char *argv[])
 {
-	int socketdescriptor,length;
+	int socketdescriptor,connectiondescriptor,flag=0;
+	//unsigned int ipaddress;
 	struct sockaddr_in servaddr;
 	memset(&servaddr,0,sizeof(servaddr));	
 	if(argc>1)
 		{
+			printf("%s\n",argv[1]);
 			servaddr.sin_addr.s_addr=inet_addr(argv[1]);
 		}
 	else
 		{
 		 	servaddr.sin_addr.s_addr=inet_addr("127.0.0.1");
 		}
-	socketdescriptor=socket(AF_INET,SOCK_DGRAM,0);							//step 1- Creating a socket and returning it's ID i.e. Socketdescriptor
-	printf("Descriptor:%d\n",socketdescriptor);
+	socketdescriptor=socket(AF_INET,SOCK_STREAM,0);
 	if(socketdescriptor==-1)
 	 {
 		printf("socket creation failed...\n");
@@ -173,16 +54,15 @@ int main(int argc,char *argv[])
 	else
 		printf("Socket successfully created..\n");
 	servaddr.sin_family=AF_INET;
-	if(argc>2)
-	{
-		printf("ARGS:%d\n",atoi(argv[2]));
-		servaddr.sin_port=htons(atoi(argv[2]));		//for taking port from user	
-	}
+	servaddr.sin_port=htons(PORT);
+	
+	if(connect(socketdescriptor,(struct sockaddr *)&servaddr,sizeof(servaddr))!=0)
+	 {
+		printf("connection with the server failed...:%d\n",connect(socketdescriptor,(struct sockaddr *)&servaddr,sizeof(servaddr)));
+		exit(0);
+	 }
 	else
-	{
-		servaddr.sin_port=htons(PORT);
-	}
-	length=sizeof(struct sockaddr_in);
-	communicate(socketdescriptor,servaddr,length);											//function for sending and receiving data from server.
-	close(socketdescriptor);											//closing the socket after the communcation completes.
-} 
+		printf("connected to the server..\n");
+	communicate(socketdescriptor);
+	close(socketdescriptor);
+}
--- server.c
+++ server.c
@@ -5,133 +5,39 @@
 #include<netdb.h>
 #include<stdlib.h>
 #include<string.h>
-#define MAX 200
-#define DATA 196
-#define PORT 43455
-struct packet
-{
-	int seqno;
-	char data[DATA];
-};
-
-
-void communicate(int socketdescriptor,struct sockaddr_in cli,int len)
+#define MAX 256
+#define PORT 43454
+void communicate(int sockfd)
 {
 	char buffer[MAX];
-	static int framecounter;	
-	FILE *fp;
-	char c;
-	int n,a,i,flag;
-	n=a=i=flag=0;
-	framecounter=1;	
-	struct packet NewPacket;
-	a = recvfrom(socketdescriptor,(void *)&NewPacket,sizeof(buffer),0,(struct sockaddr *)&cli,&len);
-	char filename[80];
-	
-	printf("a is:%d\n",a);	
-	if(a<0)
-		printf("Problem in receiving!!!\n");
-	else
- 	{
-			while(NewPacket.seqno!=-1)
-			{	
-		
-				switch(NewPacket.seqno)
-				{
-				  	case 0:
-							
-							strcpy(filename,NewPacket.data);				
-							fp=fopen(NewPacket.data,"w");
-							printf("Contents are:-\n");
-							printf("1.Seq No:-%d\n",NewPacket.seqno);
-							printf("2.data:-%s\n",NewPacket.data);
-							fclose(fp);
-		
-						break;
-					case 1: 
-							i=0;
-							fp=fopen(filename,"a+");
-							printf("Inside Case 1:\n");
-							/*while(i<DATA)
-							{
-							//	printf("-----------------------\n");
-							//	printf("%c",NewPacket.data[i]);	
-							//	printf("-----------------------\n");
-								fputc(NewPacket.data[i++],fp);
-								//i++;
-								if(i>900)
-								{
-									printf("Infinite Loop...Server will exit now");
-									break;
-								}
-								if(i==DATA)
-								{
-									printf("i is:%d\n",i);								
-									break;
-								}
-							}*/
-							fwrite(NewPacket.data,1,DATA-1,fp);	
-							fclose(fp);
-							framecounter++;
-						break;
-					default:
-						printf("Inside Default!!\n");	
-						flag=1;
-						break;
-				}
-				if(flag==1)
-				{
-					break;
-				}	
-				a = recvfrom(socketdescriptor,(void *)&NewPacket,sizeof(struct packet),0,(struct sockaddr *)&cli,&len);
-				if(a<0)
-				{
-					printf("Error while Receiving!!\n");
-				}
-				else
-				{
-					printf("Frame %d received successfully\n",framecounter);
-				}
-				printf("Received Data is:%s\n",NewPacket.data);
-				printf("Contents are:-\n");
-				printf("1.Seq No:-%d\n",NewPacket.seqno);
-				printf("2.data:-%s\n",NewPacket.data);	
-			 }
-		
-	
-	}
-	//fclose(fp);		
-        		
-
-			
-	/*for(;;)																//server will be running continuously
+	int n,status;
+	for(;;)
 	 {
 		n=0;
 		memset(buffer,0,sizeof(buffer));
-		a = recvfrom(socketdescriptor,buffer,sizeof(buffer),0,(struct sockaddr *)&cli,&len);				//step 5 - Reading the data sent by client.	
-		if(a<0)
-			printf("Receive Failed!!");	
-		printf("\n");
+		read(sockfd,buffer,sizeof(buffer));
 		printf("\t\t\tFrom client: %s\nTo client : ",buffer);
 		memset(buffer,0,MAX);
 		while((buffer[n++]=getchar())!='\n');
-		a = sendto(socketdescriptor,buffer,sizeof(buffer),0,(struct sockaddr *)&cli,len);				//step 6 - Sending the message to client.		
-		if(a<0)		
-			printf("Sending Failed!!\n");								
+		status=write(sockfd,buffer,sizeof(buffer));
+		if(status==-1)
+		{
+		  printf("-------------ERROR--------------\n");
+		  printf("Client exited or error in communication!\n");
+		}		
 		if(strncmp("exit",buffer,4)==0)
 		 {
-			printf("Server will Exit...end of communication\n");
+			printf("Server Exit...\n");
 			break;
 		}
-	 }*/
+	 }
 }
 
-int main(int argc,char *argv[])
+int main()
 {
-	int socketdescriptor,len;
+	int socketdescriptor,conndescriptor,len;
 	struct sockaddr_in servaddr,cli;
-
-	socketdescriptor=socket(AF_INET,SOCK_DGRAM,0);					//step 1- Creating a socket and returning it's ID i.e. Socketdescriptor
+	socketdescriptor=socket(AF_INET,SOCK_STREAM,0);
 	if(socketdescriptor==-1)
 	 {
 		printf("socket creation failed...\n");
@@ -139,26 +45,33 @@ int main(int argc,char *argv[])
 	 }
 	else
 		printf("Socket successfully created..\n");
-	memset(&servaddr,0,sizeof(servaddr));								// filling data in structure sockaddr_in.
+	memset(&servaddr,0,sizeof(servaddr));	
 	servaddr.sin_family=AF_INET;
-	servaddr.sin_addr.s_addr=htonl(INADDR_ANY);							// host to network byte conversion for maintaining uniformity inIP address.
-	if(argc>1)
-	{
-		servaddr.sin_port=htons(atoi(argv[1]));		//for taking port from user	
-	}
-	else
-	{
-		printf("No port provided...hence default port 43454 will be used\n");
-		servaddr.sin_port=htons(PORT);									// host to network byte conversion for maintaining uniformity in port no.
-	}
-	if((bind(socketdescriptor,(struct sockaddr *)&servaddr, sizeof(servaddr)))!=0)					//Step 2- binding socket to the IP and port provided. 
+	servaddr.sin_addr.s_addr=htonl(INADDR_ANY);
+	servaddr.sin_port=htons(PORT);
+	if((bind(socketdescriptor,(struct sockaddr *)&servaddr, sizeof(servaddr)))!=0)
 	 {
 		printf("socket bind failed...\n");
 		exit(0);
 	 }
 	else
-		printf("Socket successfully binded..waiting for clients to connect..\n");
-	len=sizeof(struct sockaddr_in);
-        communicate(socketdescriptor,cli,len);									//function for sending and receiving data from client
-	close(socketdescriptor);									//closing the socket after the communcation completes.
+	printf("Socket successfully binded..\n");
+	if((listen(socketdescriptor,5))!=0)
+	 {
+		printf("Listen failed...\n");
+		exit(0);
+	 }
+	else
+		printf("Server listening..\n");
+	len=sizeof(cli);
+	conndescriptor=accept(socketdescriptor,(struct sockaddr *)&cli,&len);
+	if(conndescriptor<0)
+	 {
+		printf("server acccept failed...\n");
+		exit(0);
+	 }
+	else
+		printf("server acccept the client...\n");
+	communicate(conndescriptor);
+	close(socketdescriptor);
 }
